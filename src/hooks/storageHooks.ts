13b69b32f2bc7fd3139b281a53881381c769cb81
codyduong <duongcody507@gmail.com>
2021-04-21 09:51:03 -0500

    Added sessionStorage

--- src/hooks/storageHooks.ts
+++ src/hooks/storageHooks.ts
@@ -15,3 +15,19 @@ export function useLocalStorage<S>(name: string) {
 
   return t
 }
+
+function getSessionStorage<S>(name: string): any {
+  // @ts-ignore : JSON.parse only accepts string, but nothing bad happens if null is passed, hence this.
+  const _: S = JSON.parse(sessionStorage.getItem(name))
+  return _
+}
+
+export function useSessionStorage<S>(name: string) {
+  const t:[S, React.Dispatch<React.SetStateAction<S>>] = useState(getSessionStorage<S>(name))
+
+  useEffect(() => {
+    sessionStorage.setItem(name, JSON.stringify(t[0]))
+  }, [name, t]);
+
+  return t
+}
