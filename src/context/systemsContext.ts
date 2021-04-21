d33bfa27808ec12dc0afc0c153acb84af6cb2bdc
codyduong <duongcody507@gmail.com>
2021-04-21 13:09:06 -0500

    Used proper SystemsReponse import

--- src/context/systemsContext.ts
+++ src/context/systemsContext.ts
@@ -2,9 +2,7 @@ import { createContext } from "react";
 import { System } from "spacetraders-sdk/dist/types";
 
 //It seems SystemsResponse from spacetraders is wrong, so here is mine
-export interface SystemsResponse {
-  systems: System[]
-}
+import { SystemsResponse } from "../spacetraders/spacetraders"
 
 export const systemsDefault: SystemsResponse = {
   systems: []
