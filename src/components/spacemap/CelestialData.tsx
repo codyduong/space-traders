e86dbdfa45d29df6c86ebe28e2d4c200cade469b
codyduong <duongcody507@gmail.com>
2021-04-23 11:05:38 -0500

    Draggable bounded by vw and vh

--- src/components/spacemap/CelestialData.tsx
+++ src/components/spacemap/CelestialData.tsx
@@ -1,7 +1,8 @@
-import { useState, useContext, useEffect } from "react"
+import { useState, useContext, useEffect, useRef } from "react"
 import useWindowDimensions from "../../hooks/useWindowDimensions"
 import { celestialContext } from "./celestialContext"
 import Draggable from "react-draggable"
+import { animated, useSpring } from "react-spring"
 
 export interface dataActiveState {
   active: boolean,
@@ -31,6 +32,18 @@ const CelestialData = (props: any) => {
   useEffect(() => {
     setCelestialIndexer(setActive, props.index, "setCelestialDataActive")
   }, [])
+
+  const [shown, setShown] = useState<boolean>(false)
+  const animate = useSpring({
+    opacity: 1,
+    reset: false,
+    delay: 0,
+    reverse: !shown,
+    overflow: 'hidden',
+    maxWidth: '0em',
+    maxHeight: '0em',
+    from: { maxWidth: '30em', maxHeight: '30em'},
+  })
   
   const style = {
     // transform: `translate(${active?.x/xy.x * 100}vw, ${active?.y/xy.y * 100}vh)`,
@@ -38,18 +51,42 @@ const CelestialData = (props: any) => {
     opacity: active?.active ? 1 : 0,
   }
 
+  const ref = useRef<HTMLDivElement>(null!)
   return (
-    <Draggable positionOffset={{x: ratio?.x*width, y: ratio?.y*height}}>
+    <Draggable 
+      positionOffset={{x: ratio?.x*width, y: ratio?.y*height}} 
+      handle="span"
+      bounds={{
+        top: - ratio?.y*height, 
+        right: width - ratio?.x*width - ref?.current?.offsetWidth,
+        bottom: height - ratio?.y*height - ref?.current?.offsetHeight, 
+        left: - ratio?.x*width}}
+    >
       <div
+        ref={ref}
         className="SpaceMap_SelectedLocation"
         id={props.loc.name}
         style={style}
-        onClick={()=>{
+        onClick={() => {
           //setActive({active: !active.active, x: 0, y: 0})
           //celestialIndexer[props.index].setCelestialActive(false)
         }}
       >
-        {String(`[${props.loc.symbol}] ${props.loc.name}`)} <br></br>
+        <span>
+          <div className="SpaceMap_Tab">
+            <div className="SpaceMap_Name">
+              {String(`[${props.loc.symbol}] ${props.loc.name}`)}
+            </div>
+            <div className="SpaceMap_NavBar">
+              <div className="SpaceMap_Mini">
+                {shown ? '-' : '☐'}
+              </div>
+              <div className="SpaceMap_Quit">
+                X
+              </div>
+            </div>
+          </div>
+        </span>
         {String(`(${props.loc.x}, ${props.loc.y})`)}
       </div>
     </Draggable>
